e489555423c3c990823b172817ffb026f58df1a5
marwan-unity <73781210+marwan-unity@users.noreply.github.com>
2022-05-18 00:55:45 +0100

    Update led1.ino

--- led1/led1.ino
+++ led1/led1.ino
@@ -1,17 +1,17 @@
-int buttonValue1 = 0; // si les on a appuyé sur les boutons
+int buttonValue1 = 0; // si on a appuyé sur les boutons
 int buttonValue2 = 0;
 int button1 = 10; //entrée
 int button2 = 11; //sorities
 
 int greenLed1 = 12; //entrée 
 int greenLed2 = 13; //entrée
-int greenLedValue1 = 0; //si le LED vert est allumé ou non 
+int greenLedValue1 = 0; //si la LED vert est allumé ou non 
 int greenLedValue2 = 0;
 
-int ledsArr[5] = {4,5,6,7,8}; // la list des led utilisée (sorties)
-int delayValue; // la vitesse des led
+int ledsArr[5] = {4,5,6,7,8}; // la liste des led utilisée (sorties)
+int delayValue; // la vitesse des leds
 int buttonPressedDelay = 2000;
-int speedAdded = 20; // le valeur ajouté a cette vitesse
+int speedAdded = 20; // le valeur ajoutée a cette vitesse
 
 bool gameIsStarted = false; 
 
@@ -19,7 +19,7 @@ bool gameIsStarted = false;
 void setup() {
   pinMode(button1, INPUT); //initialiser les boutons comme des entrées
   pinMode(button2, INPUT);
-  pinMode(greenLed1, INPUT);// initialiser les leds verts come des entées
+  pinMode(greenLed1, INPUT);// initialiser les leds verts comme des entées
   pinMode(greenLed2, INPUT);
   
   for(int i = 0; i <= 4; i++){
@@ -42,8 +42,8 @@ void loop() {
       buttonValue1 = digitalRead(button1);
       buttonValue2 = digitalRead(button2);
       
-      digitalWrite(ledsArr[i], HIGH); //allumé le premier led
-      delay(delayValue); // attends 250 ms
+      digitalWrite(ledsArr[i], HIGH); //allumer le premier led
+      delay(delayValue); // attendre 250 ms
       
       if(buttonValue1 != 0 || buttonValue2 != 0){  //si on appui sur l'un des boutton on attends 2s
           delay(buttonPressedDelay);
